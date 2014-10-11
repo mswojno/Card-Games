b3432120fe5ad76b699f633b6c5cddd3a08994e0
MATTHEW S WOJNO <MatthewSWojno@MATTHEWs-MacBook-Pro.local>
2014-10-11 08:32:59 -0400

    Initial Commit

---
+++ Card Games/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Card Games
+//
+//  Created by MATTHEW S WOJNO on 10/11/14.
+//  Copyright (c) 2014 Matthew Wojno. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
